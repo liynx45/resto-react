28242094ebf23bac951b37d3959393b411ce7c97
Iqbal Bahtiar <iqbalbahtiar045@gmail.com>
2023-11-14 15:29:48 +0700

    nav fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         <nav className={`absolute w-full top-0 z-50`}>
             {toggle ? <Sidebar setToggle={setToggle} /> : (
                 <div className="container">
-                    <div className="w-ful px-2 md:px-12 flex justify-between items-center py-6 bg-primaryColor">
+                    <div className="w-full px-2 md:px-12 flex justify-between items-center py-6 bg-primaryColor">
                         <div className="flex gap-2 md:gap-8 items-center text-white">
                             <Link to={"/"} className="md:text-2xl text-base relative">
                                 <div className="w-1/2 h-[150%] absolute border border-white left-1/2 translate-x-[-50%] top-1/2 translate-y-[-50%] "></div>
